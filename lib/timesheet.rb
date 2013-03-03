a32b6dea4fb7bee2cdea272abe7bc37c61f20d12
Guido De Rosa <job@guidoderosa.net>
2013-03-03 22:47:44 +0100

    stub usage of patched google_drive (recursion)

--- lib/timesheet.rb
+++ lib/timesheet.rb
@@ -1,8 +1,12 @@
+GOOGLE_DRIVE_FORK = File.join File.dirname(__FILE__), '../google-drive-ruby'
+GOOGLE_DRIVE_FORK_LIB = File.join GOOGLE_DRIVE_FORK, 'lib'
+$LOAD_PATH.unshift GOOGLE_DRIVE_FORK_LIB if Dir.exists? GOOGLE_DRIVE_FORK_LIB
+
 require 'extensions/date'
 require 'google_drive'
 
 class Timesheet
-  DEFAULT_PATH = 'timesheet'
+  DEFAULT_PATH = ['subdir', 'a', 'sheet']
 
   def initialize(h={:configfile => "#{ENV['HOME']}/.timesheet.yml"})
     @config = YAML.load File.read h[:configfile] # TODO: not from file
@@ -16,3 +20,4 @@ class Timesheet
     @google_drive.send id, *args, &blk
   end
 end
+
